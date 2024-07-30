74ada18c1078a0cb3e6cfe182f1e446981f57e61
Luidooo <eng.limaluis@gmail.com>
2024-07-30 08:13:31 -0300

    neovim dotfiles

---
+++ private_dot_config/nvim/lua/custom/plugins/nerdtree.lua
@@ -0,0 +1,5 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"preservim/nerdtree",
+	},
+}
---
+++ private_dot_config/nvim/lua/custom/plugins/wakatime.lua
@@ -0,0 +1,5 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"wakatime/vim-wakatime",
+	},
+}
