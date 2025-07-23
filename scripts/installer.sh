c8058090a11dec190a859e4cb2a9b33e3d7d3dbe
Luis Eduardo Lima <luis.lima@hardware.org.br>
2025-07-23 19:36:11 -0300

    installer e tals

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -4,9 +4,11 @@
 ERROR="-1"
 SUCESS="0"
 INFO="1"
+LINE="======================================================="
 
-DOTFILES="$HOME$USER/.config/dotfiles/"
-CONFIG_DIR="$HOME$USER/.config/"
+DOTFILES="$HOME/.config/dotfiles/"
+CONFIG_DIR="$HOME/.config/"
+GH_USER="Luidoo"
 
 print() {
   local GREEN='\033[0;32m'
@@ -15,31 +17,52 @@ print() {
   local NO_COLOR='\033[0m'
 
   if [ $1 -eq "$ERROR" ]; then
-    echo -e "${RED}$2${NO_COLOR}"
+    echo -e "${RED}$LINE${NO_COLOR}"
+    echo -e "${RED}[ERROR] - $2${NO_COLOR}"
+    echo -e "${RED}$LINE${NO_COLOR}"
 
   else
     if [ $1 -eq "$SUCESS" ]; then
-      echo -e "${GREEN}$2${NO_COLOR}"
+      echo -e "${GREEN}$LINE${NO_COLOR}"
+      echo -e "${GREEN}[SUCESS] - $2${NO_COLOR}"
+      echo -e "${GREEN}$LINE${NO_COLOR}"
 
     else
-      echo -e "$2"
+      echo $LINE
+      echo -e "[INFO] - $2"
+      echo $LINE
 
     fi
   fi
 }
 
+verify() {
+  if [ $? -eq 0 ]; then
+    print $SUCESS "$1"
+  else
+    print $ERROR "Error! See the last [INFO] for more information!"
+    echo $2
+  fi
+
+}
+
 clone_dotfiles() {
+  print $INFO "Cloning the $GH_USER dotfiles from github..."
   rm -rf $DOTFILES
-  git clone https://github.com/Luidooo/dotfiles.git $DOTFILES
+  git clone "https://github.com/$GH_USER/dotfiles.git" $DOTFILES
+  verify "Dotfiles Cloned!"
 }
 
 docker() {
+  print $INFO "Downloanding Docker"
   sudo install -m 0755 -d /etc/apt/keyrings
   sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
   sudo apt update
   sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
   sudo usermod -aG docker ${USER}
+  verify "Docker Downloaded!"
+
 }
 
 #virtualization() {
@@ -48,10 +71,11 @@ docker() {
 #}
 
 install_nvim() {
+  print $INFO "Setting Neovim"
   sudo snap install neovim --classic
   rm -rf "$CONFIG_DIR/nvim"
   cp -r "$DOTFILES/nvim/" "$CONFIG_DIR/nvim/"
-
+  verify "Neovim configured!"
 }
 
 #install_vim() {
@@ -75,9 +99,7 @@ utilities() {
 }
 
 main() {
-  print $ERROR "ai toma"
-  print $SUCESS "ai toma"
-  print $INFO "ai toma"
+  clone_dotfiles
 }
 
 main
