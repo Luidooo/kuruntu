863caa392a1e8760e23f928c78dd3972b52e9011
Luis Eduardo Lima <luis.lima@hardware.org>
2025-09-03 16:59:48 -0300

    feat: date box function

--- dots/dot_bash/function.sh
+++ dots/dot_bash/function.sh
@@ -21,3 +21,77 @@ git_branch_name
 httpcats() {
   google-chrome "https://http.cat/$1"
 }
+
+date_box() {
+  local date_output
+  if [ -z "$1" ]; then
+    date_output=$(date)
+  else
+    date_output=$(date +"$1")
+  fi
+
+  local length=${#date_output}
+
+  local top_border="┌"
+  for ((i = 0; i < length + 2; i++)); do
+    top_border+="─"
+  done
+  top_border+="┐"
+
+  local middle="│ $date_output │"
+
+  local bottom_border="└"
+  for ((i = 0; i < length + 2; i++)); do
+    bottom_border+="─"
+  done
+  bottom_border+="┘"
+
+  echo "$top_border"
+  echo "$middle"
+  echo "$bottom_border"
+}
+
+date_box_loop() {
+
+  if [ "$1" = "" ]; then
+    local format="%a %d %H:%M:%S"
+  else
+    local format="$1"
+  fi
+  local refresh="${2:-1}"
+
+  trap 'echo "Exiting date loop"; return 0' INT
+
+  while true; do
+    clear
+
+    local date_output
+    if [ -z "$format" ]; then
+      date_output=$(date)
+    else
+      date_output=$(date +"$format")
+    fi
+
+    local length=${#date_output}
+
+    local top_border="┌"
+    for ((i = 0; i < length + 2; i++)); do
+      top_border+="─"
+    done
+    top_border+="┐"
+
+    local middle="│ $date_output │"
+
+    local bottom_border="└"
+    for ((i = 0; i < length + 2; i++)); do
+      bottom_border+="─"
+    done
+    bottom_border+="┘"
+
+    echo "$top_border"
+    echo "$middle"
+    echo "$bottom_border"
+
+    sleep "$refresh"
+  done
+}
