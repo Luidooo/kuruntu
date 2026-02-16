90af4fb1576b3badbdc4633810c59c27adc8b14f
Luis Eduardo Lima <eng.limaluis@gmail.com>
2026-02-16 19:05:33 -0300

    wip: change how install google chrome, using oficial method

---
+++ install/applications/chrome2.sh
@@ -0,0 +1,8 @@
+  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+  sudo apt update
+  sudo apt install -y google-chrome-stable
+
+
